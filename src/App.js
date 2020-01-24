b8c4fc120305a773bc9e16c9580163b41f7f633a
carlsachs <sachscarl@gmail.com>
2020-01-23 23:27:23 -0500

    finished MVP

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import Grid from "./components/grid";
 import styled from 'styled-components';
 
 const DivWrap = styled.div `
-    width: 70%;
+    width: 80%;
     margin: 0 auto;
     border: 1px solid black;
+    h2 {
+      color: maroon;
+      width: 20%;
+      margin: 0 auto;
+      text-decoration: underline;
+    }
+    p {
+      color: darkred;
+    }
+    img {
+      width: 100%;
+    }
 `;
 
 
